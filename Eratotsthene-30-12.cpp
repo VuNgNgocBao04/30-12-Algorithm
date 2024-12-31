e71f9992f77426f940d3701e8d2485644a24a34d
VuNgNgocBao04 <jackoldbao@gmail.com>
2024-12-31 11:34:58 +0700

    Algorithm

--- Eratotsthene-30-12.cpp
+++ Eratotsthene-30-12.cpp
@@ -6,7 +6,7 @@ int main() {
 	bool S[n+5];
 	fill(S,S+n+1,true);
 	for(int i=2;i<=n;i++) {
-		cout<<i<<" ";
+		if(S[i]) cout<<i<<" ";
 		for(int j=i*i;j<=n;j+=i) S[j]=false;
 	}
 }
---
+++ sinhtapcon-30-12.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+void sinh(int n,int a[],bool &ok) {
+	int i=n;
+	while(i>=0 && a[i]==1) {
+		a[i]=0;
+		i--;
+	}
+	if(i<0) ok=false;
+	else a[i]=1;
+}
+int main() {
+	int n;
+	cin>>n;
+	int a[n]={0};
+	int i=n;
+	bool ok=true;
+	while(ok) {
+		for(int i=0;i<n;i++) cout<<a[i];
+		cout<<"\n";
+		sinh(n,a,ok);
+	}
+}
